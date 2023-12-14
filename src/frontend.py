ea50dd43741c7eaafc77f35b6b1b1bfb4b0868c8
Nikolay <kranvis2@gmail.com>
2023-12-14 18:18:12 +0100

    fix previous skeleton commit

--- src/frontend.py
+++ src/frontend.py
@@ -1,16 +1,60 @@
-from flask import Flask, request
+# app.py
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-
-with open("templates/login.html", "r", encoding="utf-8") as f:
-    login = f.read()
+app.config['SECRET_KEY'] = ''  # put here JWT secret key
+app.config['postgresql.sql'] = 'postgresql://postgres:example@localhost/postgres'
+db = SQLAlchemy(app)
 
 @app.route('/')
-def index():
-    document = login.replace("{tag}", "Hello!")
+def home():
+    return 'test'
 
-    return document
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-@app.route('/favicon.ico')
-def favicon():
-    return ''
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists. Please choose a different one.', 'danger')
+        else:
+            hashed_password = generate_password_hash(password, method='sha256')
+            new_user = User(username=username, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registration successful! Please log in.', 'success')
+            return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            flash('Login successful!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Login failed. Please check your username and password.', 'danger')
+
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' in session:
+        return f'Welcome to the dashboard, User #{session["user_id"]}!'
+    else:
+        return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
