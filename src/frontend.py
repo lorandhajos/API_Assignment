8f7931158fa8b1ada0bf2a887fa928645dd5b611
Nikolay <kranvis2@gmail.com>
2024-01-20 17:22:28 +0100

    finished with a graph

--- src/frontend.py
+++ src/frontend.py
@@ -40,58 +40,47 @@ def login():
 @app.route('/dashboard')
 def dashboard():
     if 'access_token' in session:
-        api_response = requests.get("http://localhost:5000/api/v1/views_report_films", headers={"Authorization": f"Bearer {session['access_token']}"})
-        
-        if api_response.status_code == 200:
-            x = [
-                {'views': 70, 'title': "hello"},
-                {'views': 20, 'title': "world"},
-                {'views': 10, 'title': "foo"},
-            ]
+        api_response_views = requests.get("http://localhost:5000/api/v1/views_report_films_views", headers={"Authorization": f"Bearer {session['access_token']}"})
+        api_response_title = requests.get("http://localhost:5000/api/v1/views_report_films_names", headers={"Authorization": f"Bearer {session['access_token']}"})
 
+        if api_response_views.status_code and api_response_title.status_code == 200:
+            
             # Filter data based on keywords
-            keyword = request.args.get('title', default=None)
-            if keyword:
-                filtered_data = [item for item in x if keyword.lower() in item['title'].lower()]
-            else:
-                filtered_data = x
-
+            # keyword = request.args.get('title', default=None)
+            # if keyword:
+            #     filtered_data = [item for item in x if keyword.lower() in item['title'].lower()]
+            # else:
+            #     filtered_data = x
+            filtered_data_views = api_response_views.json()
+            filtered_data_title = api_response_title.json()
             # Create Bokeh plot
-            # p = figure(height=350, sizing_mode="stretch_width")
-            # p.circle([i for i in range(len(filtered_data))], [item['views'] for item in filtered_data], size=20, color="navy", alpha=0.5)
-            # graph = figure(title = "Bokeh Vertical Bar Graph")
-            # graph.xaxis.axis_label = "title"
-            # graph.yaxis.axis_label = "views"
-            #x = ['A', 'B', 'C', 'D', 'E'] 
-            x = []
+            movie_title = []
+            for item in filtered_data_title:
+                movie_title.append(item['title'])
 
-            for item in filtered_data:
-                x.append(item['title'])
+            movie_views = []
+            for item in filtered_data_views:
+                movie_views.append(item['views'])
 
-            #y = [10, 20, 30]
-            y = []
-
-            for item in filtered_data:
-                y.append(item['views'])
-
-            source = ColumnDataSource(data=dict(x=x, y=y))
-            p = figure(x_range=x)
+            source = ColumnDataSource(data=dict(x=movie_title, y=movie_views))
+            p = figure(x_range=movie_title)
             p.vbar(x='x', top='y', width=0.9, source=source) 
             hover = HoverTool(tooltips=[("Value", "@y")]) 
             p.add_tools(hover) 
             
-            for item in filtered_data:
-                print(item)
-                print(item['title'])
-                print(item['views'])
+            # for item in filtered_data:
+            #     print(item)
+            #     print(item['title'])
+            #     print(item['views'])
                         # Generate Bokeh components
             script, div = components(p)
 
             # Pass data to the template
             output_file("dashboard.html")
-            return render_template('dashboard.html', script=script, div=div, data=filtered_data, username=session.get('username'))
+            return render_template('dashboard.html', script=script, div=div, data=filtered_data_views, username=session.get('username'))
         else:
-            print(api_response.status_code)
+            print(api_response_title.status_code)
+
             flash('Error fetching data from the API', 'danger')
             return "well, not well"
     else:
